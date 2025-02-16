7ae9a86abfe4edf2d787909539deed319e0a961a
Oyedelehabeeb <hoyedeley4@gmail.com>
2025-02-16 16:12:41 +0100

    Implement animated product grid and add image URL utility

--- components/ProductThumb.tsx
+++ components/ProductThumb.tsx
@@ -1,3 +1,4 @@
+import { imageUrl } from "@/lib/imageUrl";
 import { Product } from "@/sanity.types";
 import Image from "next/image";
 import Link from "next/link";
@@ -10,7 +11,7 @@ export default function ProductThumb({ product }: { product: Product }) {
       className={`group flex flex-col bg-white rounded-lg border border-gray-200 shadow-sm hover:shadow-md transition-all duration-200 overflow-hidden ${isOutOfStock ? "opacity-50" : ""}`}
     >
       <div className="relative aspect-square w-full h-full overflow-hidden">
-        {/* {product.image && (
+        {product.image && (
           <Image
             className="object-contain transition-transform duration-300 group-hover:scale-105"
             src={imageUrl(product.image).url()}
@@ -18,7 +19,7 @@ export default function ProductThumb({ product }: { product: Product }) {
             fill
             sizes="(max-width:768px) 100vw, (max-width:1200px) 50vw 33vw"
           />
-        )} */}
+        )}
 
         {isOutOfStock && (
           <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
